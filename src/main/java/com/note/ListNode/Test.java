28fbc00d32c97698027cf08c6b5f9a2457a4ffa0
wuyh-e <wuyh-e@glodon.com>
2024-01-12 14:43:13 +0800

    First Commit!

---
+++ src/main/java/com/note/ListNode/Test.java
@@ -0,0 +1,11 @@
+package com.note.ListNode;
+
+/**
+ * @author wuyh-e
+ * @date 2024/1/12
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
